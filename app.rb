c0430fa89e102b2f53159f3bb38f296aa65dd9aa
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-20 07:20:53 -0700

    add rb files for parcel, parcels, and new_parcel

--- app.rb
+++ app.rb
@@ -9,6 +9,15 @@ get '/' do
   erb(:parcel)
 end
 
+get('/parcels/new') do
+  erb(:new_parcel)
+end
+
+get('/parcels/:id') do
+  @parcel = Parcel.find(params[:id].to_i())
+  erb(:parcel)
+end
+
 post '/parcels' do
   name = params[:parcel_name]
   width = params[:parcel_width]
@@ -16,6 +25,8 @@ post '/parcels' do
   height = params[:parcel_height]
   weight = params[:parcel_weight]
   parcel = Parcel.new
+  parcel.save
+  @parcels = Parcel.all
   erb(:parcels)
 end
 

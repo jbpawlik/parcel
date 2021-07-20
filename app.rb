7b2567ef40004d3c1c0b188b601658505145ee17
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-20 07:39:10 -0700

    add patch and delete requests to app.rb

--- app.rb
+++ app.rb
@@ -5,8 +5,15 @@ require('pry')
 also_reload('lib/**/*.rb')
 
 get '/' do
+  name = params[:parcel_name]
+  width = params[:parcel_width]
+  length = params[:parcel_length]
+  height = params[:parcel_height]
+  weight = params[:parcel_weight]
+  parcel = Parcel.new('name', 'width', 'length', 'height', 'weight', 0)
+  parcel.save
   @parcels = Parcel.all
-  erb(:parcel)
+  erb(:parcels)
 end
 
 get('/parcels/new') do
@@ -16,6 +23,11 @@ end
 get('/parcels/:id') do
   @parcel = Parcel.find(params[:id].to_i())
   erb(:parcel)
+end
+
+get('/parcels/:id/edit') do
+  @parcel = Parcel.find(params[:id].to_i())
+  erb(:edit_parcel)
 end
 
 post '/parcels' do
@@ -30,3 +42,16 @@ post '/parcels' do
   erb(:parcels)
 end
 
+patch('/parcels/:id') do
+  @parcel = Parcel.find(params[:id].to_i())
+  @parcel.update(params[:name])
+  @parcels = Parcel.all
+  erb(:parcels)
+end
+
+delete('/parcels/:id') do
+  @parcel = Parcel.find(params[:id].to_i())
+  @parcel.delete()
+  @parcels = Parcel.all
+  erb(:parcels)
+end
--- lib/parcel.rb
+++ lib/parcel.rb
@@ -3,20 +3,47 @@ require 'pry'
 class Parcel
   attr_accessor(:width, :length, :height, :weight)
 
-  @@parcel = {}
+  @@parcels = {}
+  @@total_rows = 0
 
-  def initialize(width, length, height, weight)
+
+  def initialize(name, width, length, height, weight, id)
+    @name = name
     @width = width
     @length = length
     @height = height
     @weight = weight
+    @id = id || @@total_rows += 1
   end
 
-  def self.all()
-    @@parcel.values
+  def self.all
+    @@parcels.values()
   end
 
+  def save
+    @@parcels[self.id] = Parcel.new(self.name, self.width, self.length, self.height, self.weight, self.id)
+  end
 
-  
+  def ==(parcel_to_compare)
+    self.name() == parcel_to_compare.name()
+  end
+
+  def self.clear
+    @@parcels = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@parcels[id]
+  end
+
+  def update(name)
+    @name=name
+  end
+
+  def delete
+    @@parcels.delete(self.id)
+  end
 end
+
 
